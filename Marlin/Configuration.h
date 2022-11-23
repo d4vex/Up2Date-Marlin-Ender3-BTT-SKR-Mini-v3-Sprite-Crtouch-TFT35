6cb2406b2547c19fecce9aa5b108f95c54e50d96
d4vex <2davex@gmail.com>
2022-11-23 11:03:15 +0100

    Added PSU control

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -362,8 +362,8 @@
  * Enable and connect the power supply to the PS_ON_PIN.
  * Specify whether the power supply is active HIGH or active LOW.
  */
-//#define PSU_CONTROL
-//#define PSU_NAME "Power Supply"
+#define PSU_CONTROL
+#define PSU_NAME "Power Supply"
 
 #if ENABLED(PSU_CONTROL)
   //#define MKS_PWC                 // Using the MKS PWC add-on
